ffd01706cec7d6370eeb4f5d991f17c7e6b4343f
Manda <tigra_de@hotmail.com>
2019-11-17 21:42:18 +0700

    + Add something

--- Escapee.gd
+++ Escapee.gd
@@ -2,20 +2,23 @@ extends KinematicBody2D
 
 # Declare member variables here.
 const UP = Vector2(0, -1)
-const GRAVITY = 5
+const GRAVITY = 10
 
 var motion = Vector2()
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
+	randomize()
 	pass # Replace with function body.
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _physics_process(delta):
-	motion.y = GRAVITY
+	motion.y += GRAVITY
 	
-	#if is_on_floor():
-	#	motion.y = 0
+	if is_on_floor():
+		if Input.is_action_just_pressed("ui_up"):
+			motion.y = -700
+			motion.x = randi() % 20 + 50
 	
 	motion = move_and_slide(motion, UP)
 	pass
